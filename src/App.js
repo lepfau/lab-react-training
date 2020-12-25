6a121ab14b1e4e8e0b4ec2a048a1789c7c45710e
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2020-12-25 16:33:28 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -21,13 +21,57 @@ class Idcard extends Component {
   }
 }
 
+class Greetings extends Component {
+  render() {
+    let greetings = {
+      de: 'Hallo',
+      en: 'Hello',
+      fr: 'Bonjour',
+      es: 'Hola',
+    }
+    return (
+      <div className="box">
+        {greetings[this.props.lang]} {this.props.children}
+      </div>
+    );
+  }
+}
+
+
+class Random extends Component {
+  render() {
+    let randomNum = Math.floor(Math.random() * (this.props.max - this.props.min + 1)) + this.props.min;
+    return (
+      <div className="randompart">
+        Random value between {this.props.min} and {this.props.max} => {randomNum}
+      </div>
+    )
+  }
+}
+
+
+
+class BoxColor extends Component {
+  render() {
+
+    return (
+      <div style={{ backgroundColor: `rgb(${this.props.r}, ${this.props.g}, ${this.props.b})` }}>
+        rgb({this.props.r},{this.props.g},{this.props.b})
+
+      </div >
+    )
+
+  }
+}
+
+
 function App() {
   return (
     <div className="idcard">
       <h1> ID CARDS</h1>
       <Idcard
-        lastName="frite"
-        firstName="ss"
+        lastName="Ed"
+        firstName="Doe"
         gender="male"
         height={178}
         birth={new Date("1992-07-14")}
@@ -42,7 +86,20 @@ function App() {
         birth={new Date("1988-05-11")}
         picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
+
+      <h1>Greetings</h1>
+
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">Greg</Greetings>
+
+      <h1> Random</h1>
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
+      <h1>Color boxes</h1>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
+
   );
 }
 
