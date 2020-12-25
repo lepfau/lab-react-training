7c2cbcc8ee5fdb1f01e7af27f13c028ccfd54f2c
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2020-12-25 15:46:40 +0100

    first iteration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+
+
+class Idcard extends Component {
+  render() {
+    return <div className="card">
+      <div class="cardimg"> <img src={this.props.picture}></img> </div>
+      <div class="cardinfo">
+        <p>Last Name:{this.props.lastName} </p>
+        <p>First Name:{this.props.firstName} </p>
+        <p>Gender: {this.props.gender}</p>
+        <p>Height: {this.props.height / 100}m </p>
+        <p>Birth: {this.props.birth.toDateString()} </p>
+      </div>
+
+    </div>
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="idcard">
+      <h1> ID CARDS</h1>
+      <Idcard
+        lastName="frite"
+        firstName="ss"
+        gender="male"
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <Idcard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
     </div>
   );
 }
